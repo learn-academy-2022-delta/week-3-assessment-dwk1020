f45905eb611f8a57670e32872a5fdab793dfbac1
dwk1020 <dwk1020@aol.com>
2022-07-03 16:17:51 -0700

    week 3 complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -11,9 +11,17 @@
 // Run the file with the following command: $ yarn jest
 
 
-// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+// 1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+
+//  describe("fib"), () => {
+//      it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence."), () => {
+//          expect(fib("fibLength1").toEqual([1, 1, 2, 3, 5, 8]))
+//      }
+//  }
+
 
 const fibLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
@@ -21,13 +29,38 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+// -->Psuedocode
+// Create two sets of variables called fib and fib1.
+// Both variables will use a for loop that takes in the length of the index and out puts numbers from the Fibonacci sequence based on the length of the array provided on the const, either 6 or 10.
 
-// b) Create the function that makes the test pass.
+
+  var fib = [1, 1];
+  for(var i=fib.length; i<6; i++) {
+      fib[i] = fib[i-2] + fib[i-1];
+  }
+ var fib1 = [1, 1];
+ for(var i=fib1.length; i<10; i++) {
+     fib1[i] = fib1[i-2] + fib1[i-1];
+ }
+ console.log(fib, fib1); 
+//--->returned 
+//  [ 1, 1, 2, 3, 5, 8 ] [
+//   1,  1,  2,  3,  5,
+//   8, 13, 21, 34, 55
+// ]
 
 
-// --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+// 2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+
+//  describe("newArray", () => {
+//      it("takes in an array and returns a new array of only odd numbers sorted from least to greatest."),
+//        () => {
+//          expect(newArray("fullArray").toEqual([-9, 7, 9, 199]))
+//      }
+//  })
 
 const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 // Expected output: [-9, 7, 9, 199]
@@ -38,10 +71,34 @@ const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 
 // b) Create the function that makes the test pass.
 
+// -->Psuedocode
+// Create a function called newArray that takes an array and filters out the odd numbers using .filter, then sorts the odd numbers using .sort.  Return the sorted odd numbers using .map to ensure they are in order of value 
+
+
+function newArray(array) {
+    const odd = fullArr1.filter((x) => x % 2).sort((a,b) => a - b);
+    return fullArr1.map((x) => x % 2 ? odd.shift() : x);
+ }
+  console.log(newArray([fullArr1]))
+// ----->returned 
+// [
+//   4,     -9,    0,
+//   true,  8,     '7',
+//   'hey', 7,     9,
+//   199,   false, 'hola'
+// ]
+
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// describe("array1", () => {
+//   it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array."),
+//     () => {
+//       expect(array1("numbersToAdd1").toEqual([2, 6, 51, 60]))
+//   }
+// })
 
 const numbersToAdd1 = [2, 4, 45, 9]
 // Excpected output: [2, 6, 51, 60]
@@ -54,3 +111,23 @@ const numbersToAdd3 = []
 
 
 // b) Create the function that makes the test pass.
+
+// -->Psuedocode
+// Use let for three new arrays (array1, array2, and array3)
+// Tie each new array to the corresponding const and result.
+// Creat a for loop for each array with a starting index , array length using .length, and iterate over +1  
+
+// console log each result together or seperate
+
+
+
+ let array1 = numbersToAdd1, result1 = [];
+ let array2 = numbersToAdd2, result2 = [];
+ let array3 = numbersToAdd3, result3 = [];
+
+ for (let i = 0, s = array1[0]; i < array1.length; i++, s += array1[i]) result1.push(s);
+ for (let i = 0, s = array2[0]; i < array2.length; i++, s += array2[i]) result2.push(s);
+ for (let i = 0, s = array3[0]; i < array3.length; i++, s += array3[i]) result3.push(s);
+
+ console.log(result2, result1, result3);
+//  --->returned [ 0, 7, -1, 11 ] [ 2, 6, 51, 60 ] []
--- interview-questions.md
+++ interview-questions.md
@@ -9,59 +9,63 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: THIS is a method to identify objects and attatch or tie them to a function within React.
 
-  Researched answer:
+  Researched answer:THIS basically gives direction to the object it is attached too.  It basically tells you were to get data or value from.
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is a development tool that lets devopers see their work and any changers to their work in real time on the web.  What makes React so unique is it stores all the necessary Javascript library codes and has them ready for the developer to use when needed. 
 
-  Researched answer:
+  Researched answer:React is a front end framework that works with virtual DOM (Document Object Model) and created by Faccebook.  Basically a library of Javascript that is scalable and updates in real time.
+  React is flexible, reliable, easy to use, support driven, has a large user communkity and its easy to test.
 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: State is a required lifecycle method in a React class component becuase to ties various functions together so the desired outcome is a reality.
 
-  Researched answer:
+  Researched answer:The render() method is the only method required in a class component.
 
 
 
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is the merging of HTML and JAVASCRIPT code using the curly brackets and / .  So in a .JS file we can have both HTML and JAVASCRIPT working together on the same code line.
+  HTML is showing the basic foundation of the webpage while the JSX is taking that foundation and giving it functionality.  
 
   Researched answer:
-
+JSX is an extension of javascript that lets you write html code within javascript.  One sntax difference is tags self close in JSX.  So in 
+<div></div> in HTML is <div /> in JSX.
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is what ties all the code together in the React application.  All the files in the SRC folder are modified when running yarn.
 
-  Researched answer:
-
+  Researched answer:Yarn is basically a package manager that keeps everything organized and secure.
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: An anonymous function is not tied to a any return and or name but can be called upon later or when needed.
 
-  Researched answer:
+  Researched answer:A function without an identifier.  Not used during creation but can be called upon later.
 
 
 ## Looking Ahead: Terms for Next Week
 
-1. Conditional rendering:
+1. Conditional rendering:The ability to render different user interfaces based on if the condition is true or false.
 
-2. Object-oriented programming:
+2. Object-oriented programming:Rather than designing around functions and logic, OOP designs around data and objects.
 
-3. Ruby:
+3. Ruby:Ruby is a programing laguage that uses simple language andsyntax to hand data and logic to solve problems.
 
 4. Ruby blocks:
+Ruby blocks are chunks of code enclosed in {} and assigned a name that can be called upon by a function of the same name.
 
 5. Ruby hashes:
+Ruby hashes is like an array without the numerical indexes but instead using keys and corressponding values.
